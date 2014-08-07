bdd801f79e33d645ebfa4ac49eac98d4d05e46dd
dellheng <mrheng@163.com>
2014-08-07 16:20:19 +0800

    新增 user功能

---
+++ src/main/java/com/github/maven/demo/web/UserController.java
@@ -0,0 +1,19 @@
+package com.github.maven.demo.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping(value="/getUser.do")  
+    public ModelAndView getUser(HttpServletRequest request,HttpServletResponse response){
+    	String name = request.getParameter("name");
+    	return new ModelAndView("index","name",name);
+    }
+}
